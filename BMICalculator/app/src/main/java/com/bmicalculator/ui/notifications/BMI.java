4a560e01b8cce6d29fc7bc0d4886364468722f1d
CezG <48533560+CezG@users.noreply.github.com>
2021-04-10 00:02:01 +0200

    added switch sex button

--- BMICalculator/app/src/main/java/com/bmicalculator/ui/notifications/BMI.java
+++ BMICalculator/app/src/main/java/com/bmicalculator/ui/notifications/BMI.java
@@ -5,6 +5,7 @@ import com.bmicalculator.Person;
 public class BMI {
     private Person person;
     private double value;
+    private int level = 0;
 
     public BMI(Person person) {
         this.person = person;
@@ -19,18 +20,26 @@ public class BMI {
         this.value = value;
     }
 
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
     public String rateBmi() {
         String txt = "";
 
         while (txt.isEmpty()) {
-            if (value >= 40) return txt = "obese class 3";   //otyłość skrajna
-            if (value >= 35) return txt = "obese class 2";  //ii stopień otyłości
-            if (value >= 30) return txt = "obese class 1";  //i stopień otyłości
-            if (value >= 25) return txt = "overweight";     //nadwaga
-            if (value >= 18.5) return txt = "normal"; //wartość prawidłowa
-            if (value >= 17) return txt = "mild thinness";   //niedowaga
-            if (value >= 16) return txt = "moderate thinness"; //wychudzenie
-            if (value < 16) return txt = "severe thinness"; //wygłodzenie
+            if (value >= 40) {level = 7; return txt = "obese class 3";}   //otyłość skrajna
+            if (value >= 35) {level = 6; return txt = "obese class 2";}  //ii stopień otyłości
+            if (value >= 30) {level = 5 ;return txt = "obese class 1";}  //i stopień otyłości
+            if (value >= 25) {level = 4; return txt = "overweight"; }    //nadwaga
+            if (value >= 18.5) {level = 3; return txt = "normal"; }//wartość prawidłowa
+            if (value >= 17) {level = 2; return txt = "mild thinness";}   //niedowaga
+            if (value >= 16) {level = 1; return txt = "moderate thinness";} //wychudzenie
+            if (value < 16) {level = 0; return txt = "severe thinness" ;} //wygłodzenie
         }
         return txt;
 
--- BMICalculator/app/src/main/java/com/bmicalculator/ui/notifications/NotificationsFragment.java
+++ BMICalculator/app/src/main/java/com/bmicalculator/ui/notifications/NotificationsFragment.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,6 +24,8 @@ public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
 
+    private Switch switchSex;
+    private TextView tvSex;
     private EditText personWeight;
     private EditText personHeight;
     private TextView tvBMI;
@@ -33,6 +37,8 @@ public class NotificationsFragment extends Fragment {
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
+        switchSex = root.findViewById(R.id.switchSex);
+        tvSex = root.findViewById(R.id.textViewSex);
         personWeight = root.findViewById(R.id.personWeight);
         personHeight = root.findViewById(R.id.personHeight);
         tvBMI = root.findViewById(R.id.textViewBMI);
@@ -43,12 +49,23 @@ public class NotificationsFragment extends Fragment {
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
 
+                switchSex.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(isChecked) {
+                            tvSex.setText("Female");
+                        }else{
+                            tvSex.setText("Male");
+                        };
+                    }
+                });
+
 
                 btnCheck.setOnClickListener(new View.OnClickListener() {
                                                 public void onClick(View v) {
                                                     if (TextUtils.isEmpty(personWeight.getText())) {
                                                         personWeight.setError("The weight is required");
-                                                    }else if(TextUtils.isEmpty(personHeight.getText())){
+                                                    } else if (TextUtils.isEmpty(personHeight.getText())) {
                                                         personHeight.setError("The height is required");
                                                     } else {
                                                         int weight = Integer.parseInt(String.valueOf(personWeight.getText()));
