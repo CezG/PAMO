71a556bc5cee2eb2f99b5cbfd304d92744d712aa
CezG <48533560+CezG@users.noreply.github.com>
2021-05-15 12:07:41 +0200

    bmi calculator without flag quiz and kotlin

---
+++ BMICalculator/app/src/main/java/com/bmicalculator/ui/chart/ChartViewModel.java
@@ -0,0 +1,7 @@
+package com.bmicalculator.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class ChartViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ BMICalculator/app/src/main/java/com/bmicalculator/ui/quiz/QuizFragment.java
@@ -0,0 +1,38 @@
+package com.bmicalculator.ui.quiz;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bmicalculator.R;
+
+public class QuizFragment extends Fragment {
+
+    private QuizViewModel mViewModel;
+
+    public static QuizFragment newInstance() {
+        return new QuizFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(QuizViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
